17daa88dd8001fe73040afda8f9607a448220f63
Daniel Butum <danibutum@gmail.com>
2018-03-27 12:11:28 +0300

    Disable faster without unity

--- Source/DlgSystemEditor/DlgSystemEditor.Build.cs
+++ Source/DlgSystemEditor/DlgSystemEditor.Build.cs
@@ -11,8 +11,8 @@ public class DlgSystemEditor : ModuleRules
 		// https://docs.unrealengine.com/latest/INT/Programming/UnrealBuildSystem/Configuration/
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
         bEnforceIWYU = true;
-		MinFilesUsingPrecompiledHeaderOverride = 1;
-		bFasterWithoutUnity = true;
+		//MinFilesUsingPrecompiledHeaderOverride = 1;
+		//bFasterWithoutUnity = true;
         //PrivatePCHHeaderFile = "Private/DlgSystemEditorPrivatePCH.h";
 
 		//if ((Target.Platform == UnrealTargetPlatform.Win32 || Target.Platform == UnrealTargetPlatform.Win64)
