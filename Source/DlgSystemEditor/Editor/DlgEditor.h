8d29ddac39b7ebc07c26a780f9c4a071ba512655
Marius Petcu <marius@petcu.me>
2025-06-16 09:15:06 +0300

    Fix some warnings for Unreal 5.6 (#16)
    In my last PR I forgot to try to compile the editor module and some
    required changes slipped by

--- Source/DlgSystemEditor/Editor/DlgEditor.h
+++ Source/DlgSystemEditor/Editor/DlgEditor.h
@@ -280,7 +280,7 @@ private:
 
 	// Called to create context menu when right-clicking on graph
 	FActionMenuContent OnCreateGraphActionMenu(UEdGraph* InGraph,
-		const FVector2D& InNodePosition,
+		const FNYVector2f& InNodePosition,
 		const TArray<UEdGraphPin*>& InDraggedPins,
 		bool bAutoExpand,
 		SGraphEditor::FActionMenuClosed InOnMenuClosed);
