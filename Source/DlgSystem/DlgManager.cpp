bf1c6d9897e16555be985acd40d92e0849ff20f9
Mehdi Zakaria Benadel <mehdi.benadel@gmail.com>
2026-02-28 13:34:51 +0100

    Only detect transactional DlgParticipants

--- Source/DlgSystem/DlgManager.cpp
+++ Source/DlgSystem/DlgManager.cpp
@@ -581,7 +581,8 @@ void UDlgManager::GatherParticipantsRecursive(UObject* Object, TArray<UObject*>&
 	}
 
 	AlreadyVisited.Add(Object);
-	if (Object->GetClass()->ImplementsInterface(UDlgDialogueParticipant::StaticClass()))
+	if (Object->HasAllFlags(RF_Transactional) &&
+		Object->GetClass()->ImplementsInterface(UDlgDialogueParticipant::StaticClass()))
 	{
 		Array.Add(Object);
 	}
